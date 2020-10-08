f8bab1651c7439c5c8268af53573a2728b85f01f
Curtis Deptuck <curtdept@me.com>
2020-10-08 09:36:45 -0600

    3.8.5 sync

--- src/tools/chocolateyinstall.ps1
+++ src/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # release version of kustomize: https://github.com/kubernetes-sigs/kustomize/releases
-$version = '3.8.4'
+$version = '3.8.5'
 # pattern for archive name
 $base_name = "kustomize_v$($version)_windows_amd64"
 $zip_name = $base_name + ".tar.gz"
@@ -13,8 +13,8 @@ $exe_name = "kustomize.exe"
 $url = "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv$($version)/$($zip_name)"
 
 # use $ checksum [exe] -t=sha256
-$archive_checksum = '54D42CB8690ECB63ACD3CB302D77E24DB9EA368C4F11C0B197CFA3A0C9CE516C'
-$exe_checksum = '1C20A75425C1ED47D5DEC81794B07A257473569BED62F880A9A5DCBA5A8D3EDA'
+$archive_checksum = '0F1DD1F1A72B31EEFD715E6BD517AE161307733E65EE2E0B5BB2E360E0A29308'
+$exe_checksum = '7171E7F65661B43C3F1C1C2AFC54FEE7652BFE71787033FE1B693428CA0C2DD1'
 $checksum_type = 'sha256'
 
 # destinations
