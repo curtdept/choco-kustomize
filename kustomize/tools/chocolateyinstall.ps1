6359edca8cc56560286696b1b83754194228e1ff
Curtis Deptuck <curtdept@me.com>
2026-04-13 18:12:20 -0600

    bump 5.8.1

--- kustomize/tools/chocolateyinstall.ps1
+++ kustomize/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 # release version of kustomize: https://github.com/kubernetes-sigs/kustomize/releases
 
 # only 64bit url
-$url64 = 'https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v5.5.0/kustomize_v5.5.0_windows_amd64.zip'
+$url64 = 'https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v5.8.1/kustomize_v5.8.1_windows_amd64.zip'
 
 # use $ checksum [exe] -t=sha256
-$checksum64 = 'a19684ae51f7a768f937f713780411cfb3945339a509294504daf9b87a77b642'
+$checksum64 = '8ec7f5e815e526d4622c06df0a7793d8cfb6eb1c74f816b46166097fef8b26c6'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
